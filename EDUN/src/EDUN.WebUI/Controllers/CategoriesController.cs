346344428cea04318e4067655e9de40616181374
Luke Dol <sukh.dol77@gmail.com>
2020-06-23 11:07:12 -0600

    Added the CategoriesController with Get API.

---
+++ EDUN/src/EDUN.WebUI/Controllers/CategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using EDUN.Domain.Entities;
+using EDUN.Domain.Interfaces.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EDUN.WebUI.Controllers
+{
+    [Route("/api/categories")]
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepo;
+
+        public CategoriesController(ICategoryRepository categoryRepo)
+        {
+            _categoryRepo = categoryRepo;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Category>> GetCategories()
+        {
+            return await _categoryRepo.GetCategories(true);
+        }
+    }
+}
