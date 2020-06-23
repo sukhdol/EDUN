d19ab1789f82d143a6117949c8bcaef7cf9a0e7e
Luke Dol <sukh.dol77@gmail.com>
2020-06-23 12:59:54 -0600

    Added Application Layers DI.

---
+++ EDUN/src/EDUN.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EDUN.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
