5ae5fc67945857dffc39c695f6c20031dc2b0e1e
Luke Dol <sukh.dol77@gmail.com>
2020-06-23 12:37:34 -0600

    Added the Item Repo.

---
+++ EDUN/src/EDUN.Domain/Interfaces/Repositories/IItemRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using EDUN.Domain.Entities;
+
+namespace EDUN.Domain.Interfaces.Repositories
+{
+    public interface IItemRepository
+    {
+        void Add(Item item);
+        void Remove(Item item);
+
+        Task<Item> GetItem(int id, bool includeRelated = true);
+    }
+}
---
+++ EDUN/src/EDUN.Infrastructure/Repositories/ItemRepository.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using EDUN.Application.Common.Interfaces;
+using EDUN.Domain.Entities;
+using EDUN.Domain.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace EDUN.Infrastructure.Repositories
+{
+    public class ItemRepository : IItemRepository
+    {
+        private readonly IApplicationDbContext _applicationDbContext;
+
+        public ItemRepository(IApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+        
+        public void Add(Item item)
+        {
+            _applicationDbContext.Items.Add(item);
+        }
+
+        public void Remove(Item item)
+        {
+            _applicationDbContext.Items.Remove(item);
+        }
+
+        public async Task<Item> GetItem(int id, bool includeRelated = true)
+        {
+            if (!includeRelated)
+            {
+                return await _applicationDbContext.Items.FindAsync(id);
+            }
+
+            return await _applicationDbContext.Items
+                            .Include(i => i.Category)
+                            .SingleOrDefaultAsync(i => i.Id == id);
+        }
+    }
+}
