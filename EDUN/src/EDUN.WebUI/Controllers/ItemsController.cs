e86004c0319274e77ab264ea6cffa9fae22c96eb
Luke Dol <sukh.dol77@gmail.com>
2020-06-23 12:39:59 -0600

    Added ItemsController and CreateItem API.

---
+++ EDUN/src/EDUN.WebUI/Controllers/ItemsController.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using EDUN.Domain.Entities;
+using EDUN.Domain.Interfaces.Repositories;
+using EDUN.Domain.Interfaces.UnitOfWork;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EDUN.WebUI.Controllers
+{
+    [Route("/api/items")]
+    public class ItemsController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IItemRepository _itemRepo;
+
+        public ItemsController(IUnitOfWork unitOfWork,
+                                IItemRepository itemRepo)
+        {
+            _unitOfWork = unitOfWork;
+            _itemRepo = itemRepo;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateItem([FromBody] Item newItem)
+        {
+            _itemRepo.Add(newItem);
+            await _unitOfWork.Complete();
+
+            var itemInDb = _itemRepo.GetItem(newItem.Id);
+
+            return Ok(itemInDb);
+        } 
+    }
+}
