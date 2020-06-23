61db56b59fb6ac3e3f0f4d6ab916ed8d6a7fee7a
Luke Dol <sukh.dol77@gmail.com>
2020-06-23 16:48:41 -0600

    Added the Category Resource for mapping.

---
+++ EDUN/src/EDUN.Application/Resources/CategoryResource.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace EDUN.Application.Resources
+{
+    public class CategoryResource
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<ItemResource> Items { get; set; }
+    }
+}
