02a1b274d290911a18456d406767c55f0fd3df89
Luke Dol <sukh.dol77@gmail.com>
2020-06-23 06:17:23 -0600

    Added the ApplicationDbContext and EFCore.

---
+++ EDUN/src/EDUN.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EDUN.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+    }
+}
