f9f6fda9741d2965c083fadfcd3ce8e09c6624cb
Luke Dol <sukh.dol77@gmail.com>
2020-06-23 05:49:16 -0600

    Added Item model.

---
+++ EDUN/src/EDUN.Domain/Entities/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EDUN.Domain.Entities
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Value { get; set; }
+    }
+}
