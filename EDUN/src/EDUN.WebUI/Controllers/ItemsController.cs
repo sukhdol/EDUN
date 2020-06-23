bc2dbc942593e23d234c1bc099622b4c342244fb
Luke Dol <sukh.dol77@gmail.com>
2020-06-23 14:51:20 -0600

    Refactored CreateItem Api.

--- EDUN/src/EDUN.WebUI/Controllers/ItemsController.cs
+++ EDUN/src/EDUN.WebUI/Controllers/ItemsController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Threading.Tasks;
+using EDUN.Application.Resources;
+using EDUN.Application.Services;
 using EDUN.Domain.Entities;
 using EDUN.Domain.Interfaces.Repositories;
 using EDUN.Domain.Interfaces.UnitOfWork;
@@ -9,25 +11,19 @@ namespace EDUN.WebUI.Controllers
     [Route("/api/items")]
     public class ItemsController : Controller
     {
-        private readonly IUnitOfWork _unitOfWork;
-        private readonly IItemRepository _itemRepo;
+        private readonly IItemService _itemService;
 
-        public ItemsController(IUnitOfWork unitOfWork,
-                                IItemRepository itemRepo)
+        public ItemsController(IItemService itemService)
         {
-            _unitOfWork = unitOfWork;
-            _itemRepo = itemRepo;
+            _itemService = itemService;
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateItem([FromBody] Item newItem)
+        public async Task<IActionResult> CreateItem([FromBody] ItemResource newItemResource)
         {
-            _itemRepo.Add(newItem);
-            await _unitOfWork.Complete();
-
-            var itemInDb = _itemRepo.GetItem(newItem.Id);
-
-            return Ok(itemInDb);
+            var result = await _itemService.CreateItem(newItemResource);
+            
+            return Ok(result);
         } 
     }
 }
