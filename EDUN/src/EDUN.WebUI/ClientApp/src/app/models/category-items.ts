37e96ce2f46e6587ecffb59d448cb64ae6ca8380
Luke Dol <sukh.dol77@gmail.com>
2020-06-24 01:44:03 -0600

    Improved the logic on total calculation.

--- EDUN/src/EDUN.WebUI/ClientApp/src/app/models/category-items.ts
+++ EDUN/src/EDUN.WebUI/ClientApp/src/app/models/category-items.ts
@@ -3,13 +3,16 @@ import { Item } from './item';
 export class CategoryItem {
   id: number;
   name: string;
-  total: number;
+  total = 0;
   items: Item[];
 
   constructor(categoryItem: Partial<CategoryItem>) {
     Object.assign(this, categoryItem);
-    this.total = this.items.map(i => i.value).reduce(function(acc, cur) {
-      return acc + cur;
-    });
+
+    if (this.items.length > 0) {
+      this.total = this.items.map(i => i.value).reduce(function(acc, cur) {
+        return acc + cur;
+      });
+    }
   }
 }
