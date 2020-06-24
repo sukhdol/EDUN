92d5e7d62ca1e40590f0a9e0d28495a84d6a7d66
Luke Dol <sukh.dol77@gmail.com>
2020-06-23 18:00:41 -0600

    Added Category Item List component.

---
+++ EDUN/src/EDUN.WebUI/ClientApp/src/app/category-items-list/category-items-list.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-category-items',
+  templateUrl: './category-items-list.component.html',
+  // styleUrls: ['./category-items-list.component.css']
+})
+export class CategoryItemsListComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
