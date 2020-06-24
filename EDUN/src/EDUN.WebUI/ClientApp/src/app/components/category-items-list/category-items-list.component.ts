7c944fbf0a763708647f2e3f00d9dc065adc4cbd
Luke Dol <sukh.dol77@gmail.com>
2020-06-24 01:43:24 -0600

    Added deleteitem method and refactored ngOnInit.

--- EDUN/src/EDUN.WebUI/ClientApp/src/app/components/category-items-list/category-items-list.component.ts
+++ EDUN/src/EDUN.WebUI/ClientApp/src/app/components/category-items-list/category-items-list.component.ts
@@ -1,6 +1,7 @@
 import { CategoryItem } from './../../models/category-items';
 import {Component, OnInit} from '@angular/core';
 import { CategoriesService } from 'src/app/services/categories.service';
+import { ItemsService } from 'src/app/services/items.service';
 
 @Component({
   selector: 'app-category-items',
@@ -11,10 +12,25 @@ export class CategoryItemsListComponent implements OnInit {
   catItemList = new Array<CategoryItem>();
   grandTotal: number;
 
-  constructor(private categoriesService: CategoriesService) {
+  constructor(private categoriesService: CategoriesService,
+              private itemsServie: ItemsService) {
   }
 
   ngOnInit() {
+    this.getCategories();
+  }
+
+  deleteItem(id) {
+    if (confirm('Are you sure?'))
+    {
+      this.itemsServie.delete(id).subscribe(x => {
+        this.getCategories();
+      });
+    }
+  }
+
+  getCategories() {
+    this.catItemList = new Array<CategoryItem>();
     this.categoriesService.getCategoryItemsList().subscribe(cat => {
       this.grandTotal = 0;
       cat.forEach(ct => {
